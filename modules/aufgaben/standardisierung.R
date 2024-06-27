8c021daad5d0033e74edd78ae5461f2c52a373ac
Muhammad Taqiyuddin Ar Rofi <85993222+Arrofi07@users.noreply.github.com>
2024-06-27 11:33:19 +0200

    Add files via upload

---
+++ modules/aufgaben/standardisierung.R
@@ -0,0 +1,28 @@
+standardisierung_ui <- function(id) {
+  ns <- NS(id)
+  
+  tagList(
+    data_selector_ui(
+      id = ns("id_data_selector")
+    )
+  )
+}
+
+standardisierung_server <- function(id, .values) {
+ 
+   moduleServer(
+    id, 
+    function(input, output, session) {
+      ns <- session$ns
+      
+      data_r <- reactive({
+        data_selector_return$data_r()
+      })
+      
+      data_selector_return <- data_selector_server(
+        id = "id_data_selector",
+        .values = .values
+      )
+    }
+  )
+}
---
+++ modules/aufgaben/steepest_ascent.R
@@ -0,0 +1,28 @@
+steepest_ascent_ui <- function(id) {
+  ns <- NS(id)
+  
+  tagList(
+    data_selector_ui(
+      id = ns("id_data_selector")
+    )
+  )
+}
+
+steepest_ascent_server <- function(id, .values) {
+  
+  moduleServer(
+    id, 
+    function(input, output, session) {
+      ns <- session$ns
+      
+      data_r <- reactive({
+        data_selector_return$data_r()
+      })
+      
+      data_selector_return <- data_selector_server(
+        id = "id_data_selector",
+        .values = .values
+      )
+    }
+  )
+}
